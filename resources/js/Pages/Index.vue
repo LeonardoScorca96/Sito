1def28e11c0ed8b4568ae10489022a8f23bc4269
LeonardoScorca96 <122266658+LeonardoScorca96@users.noreply.github.com>
2023-01-09 12:47:51 +0100

    primo commit

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="container mx-auto">
+        <h1 class="text-2xl">ss{{variabile}}</h1>
+        Questa è la nuova index con inertiajs
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps(['variabile'])
+</script>
